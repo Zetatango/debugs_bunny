495e7b4c9aa672cd4589ae5745607b7870d32bb7
Brandon Vrooman <brandon.vrooman@gmail.com>
2020-02-11 10:39:33 -0500

    Initial commit

---
+++ lib/debugs_bunny.rb
@@ -0,0 +1,6 @@
+require "debugs_bunny/version"
+
+module DebugsBunny
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/debugs_bunny_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe DebugsBunny do
+  it "has a version number" do
+    expect(DebugsBunny::VERSION).not_to be nil
+  end
+end
