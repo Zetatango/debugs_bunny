fa13ccaf4d7835fd59f17b91219ec132027d607b
Brandon Vrooman <brandon.vrooman@gmail.com>
2020-02-18 16:37:13 -0500

    Add codecov support (#5)
    * Add codecov support
    
    * Remove group
    
    * Add cov groups

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,24 @@ require 'factory_bot'
 require 'debugs_bunny'
 require 'support/model_spec_helper'
 
+if ENV['COVERAGE'] || ENV['CI']
+  require 'simplecov'
+  require 'codecov'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::Codecov,
+      SimpleCov::Formatter::HTMLFormatter
+    ]
+  )
+
+  SimpleCov.start do
+    add_group 'lib', %w[lib]
+    add_group 'models', %w[lib/debugs_bunny/models]
+    add_group 'spec', %w[spec]
+  end
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
