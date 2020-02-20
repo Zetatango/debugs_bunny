b015592fa5604141543a9f6468e2c558b9139285
Brandon Vrooman <brandon.vrooman@gmail.com>
2020-02-20 11:05:33 -0500

    Update has_guid_spec.rb

--- spec/models/debugs_bunny/has_guid_spec.rb
+++ spec/models/debugs_bunny/has_guid_spec.rb
@@ -5,42 +5,63 @@ require 'models/debugs_bunny/concerns/has_guid'
 class DebugTrace < DebugsBunny::Trace; end
 
 RSpec.describe DebugsBunny::HasGuid do
-  let(:trace) { create :debug_trace }
+  context 'when creating a record' do
+    it 'generates a guid for the record' do
+      trace = create :debug_trace
+      expect(trace.guid).to start_with DebugTrace.has_guid_prefix
+    end
 
-  it 'duplicated encryption_key have no guid but get one when they are saved' do
-    duplicate = trace.dup
-    expect(duplicate.guid).to be_nil
-    expect(duplicate.id).to be_nil
-    duplicate.save!
-    expect(duplicate.guid).to start_with DebugTrace.has_guid_prefix
+    it 'allows records to specify a guid' do
+      guid = DebugTrace.has_guid_prefix + '_123'
+      trace = create :debug_trace, guid: guid
+      expect(trace.guid).to eq guid
+    end
   end
 
-  it '#to_param returns the guid' do
-    expect(trace.to_param).to equal trace.guid
+  context 'when duplicating a record' do
+    let(:trace) { create :debug_trace }
+
+    it 'generates a new guid for the duplicate record' do
+      duplicate = trace.dup
+      expect(duplicate.guid).to be_nil
+      expect(duplicate.id).to be_nil
+      duplicate.save!
+      expect(duplicate.guid).to start_with DebugTrace.has_guid_prefix
+    end
   end
 
-  it 'blank guid on create generates a guid' do
-    duplicate = trace.dup
-    duplicate.guid = ''
-    duplicate.save!
-    expect(duplicate.guid).to start_with DebugTrace.has_guid_prefix
+  describe '#save' do
+    it 'generates a new guid for the record if the guid is blank' do
+      trace = build :debug_trace, guid: ''
+      trace.save!
+      expect(trace.guid).to start_with DebugTrace.has_guid_prefix
+    end
 
-    duplicate = trace.dup
-    duplicate.guid = nil
-    duplicate.save!
-    expect(duplicate.guid).to start_with DebugTrace.has_guid_prefix
+    it 'generates a new guid for the record if the guid is nil' do
+      trace = build :debug_trace, guid: nil
+      trace.save!
+      expect(trace.guid).to start_with DebugTrace.has_guid_prefix
+    end
   end
 
-  it 'prefix is globally unique' do
+  describe '#to_param' do
+    it 'returns the guid' do
+      trace = create :debug_trace
+      expect(trace.to_param).to equal trace.guid
+    end
+  end
+
+  it 'enforces a globally unique prefix' do
     expect do
       klass = Class.new(DebugsBunny::Trace)
       klass.has_guid 'trc'
     end.to raise_error ArgumentError
   end
 
-  it 'guid cannot be changed' do
+  it 'enforces immutable guids' do
     expect do
-      trace.guid = "trc_#{SecureRandom.base58(16)}"
+      trace = create :debug_trace
+      trace.guid = DebugTrace.has_guid_prefix + "_#{SecureRandom.base58(16)}"
       trace.save!
     end.to raise_error ActiveRecord::RecordInvalid
   end
