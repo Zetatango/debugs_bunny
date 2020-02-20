351036f739b3d2ab18fc7651f8d6cc2fca2afd17
Brandon Vrooman <brandon.vrooman@gmail.com>
2020-02-20 11:30:58 -0500

    Add Rails test app

---
+++ spec/rails_test_app/config/application.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative 'boot'
+
+require 'rails'
+# Pick the frameworks you want:
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsTestApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+  end
+end
