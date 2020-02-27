027955a61ebebb73a732b1a3c08d79ea150c4b51
Brandon Vrooman <brandon.vrooman@gmail.com>
2020-02-27 11:20:38 -0500

    Fix

--- lib/debugs_bunny.rb
+++ lib/debugs_bunny.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'models/debugs_bunny/concerns/can_generate'
-require 'models/debugs_bunny/concerns/has_encrypted_attributes'
 require 'models/debugs_bunny/concerns/has_guid'
 require 'models/debugs_bunny/application_record'
 require 'models/debugs_bunny/trace'
